7c3e4578bb7d41dbf7b688c6b117c9e7253428cf
Daniel <angelicodaniel93@gmail.com>
2022-11-03 15:11:36 +0100

    inizio esercizio

---
+++ main.js
@@ -0,0 +1,26 @@
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+let simon = document.getElementById('simon-says')
+let simonNumbers = [];
+let simonNumber
+
+
+for (let i = 0; i < 5; i++){
+    simonNumber = getRandomInt(1, 100)
+    console.log(simonNumber)
+    simonNumbers.push(simonNumber)
+}
+console.log(simonNumbers)
+
+
+
+
+
+
+
+//funzione numero random
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
